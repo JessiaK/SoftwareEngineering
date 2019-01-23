0c10b0f853d33ebbd24dbe9cc50a720a762a8b00
JessiaK <jessyk520@gmail.com>
2019-01-23 16:49:57 -0500

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -7,9 +7,9 @@ public class Hello {
 	
 	public static void main(String[] args){
 		
-		Scanner x = new Scanner(System.in);
-		String name = x.nextLine();
-		System.out.println("The person running this program is:" + name);
+		Scanner x = new Scanner(System.in);  //declared and initialized the scanner
+		String name = x.nextLine(); 
+		System.out.println("The person running this program is:" + name); //printing the user's name
 	}
 	
 }
